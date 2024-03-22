0e3a7061ca7eab1d66be21e86e275f07a6effafe
harrylgd <harry.liguanda@gmail.com>
2024-03-22 03:10:38 -0400

    working on settinginfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
-import Settings from "./Settings";
+import Settings from "./components/Settings";
+import SettingsContext from "./components/SettingsContext";
 import Timer from "./components/Timer";
 import { useState } from "react";
 
 function App() {
   const [showSettings, setShowSettings] = useState(true);
 
-  return <main>{showSettings ? <Settings /> : <Timer />}</main>;
+  return( 
+  <main>
+    <SettingsContext.Provider value = {{
+      workminutes: 45,
+      breakminutes: 15,
+    }}>
+    {showSettings ? <Settings /> : <Timer />}
+    </SettingsContext.Provider>
+
+    </main>);
 }
 
 export default App;
---
+++ src/components/Settings.jsx
@@ -0,0 +1,32 @@
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import { useContext } from 'react';
+
+function Settings() {
+
+  const settingsInfo = useContext(SettingsContext);
+  
+  return (
+    <div style={{ textAlign: "left" }}>
+
+      <label>work: {settingsInfo.workMinutes}:00</label>
+      <Slider 
+        className='slider'
+        value={settingsInfo.workMinutes}
+        max={120}
+        min={1}
+      />
+
+      <label>break: {settingsInfo.breakMinutes}:00</label>
+      <Slider 
+        className='slider'
+        value={settingsInfo.breakMinutes}
+        max={120}
+        min={1}
+      />
+
+    </div>
+  );
+}
+
+export default Settings;
